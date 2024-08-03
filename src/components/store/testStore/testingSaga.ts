b2b74b45300772643985de40cc1794f3df4b1485
derrimugisha <derrimugisha@gmail.com>
2024-08-03 16:29:40 +0300

    added types in generators

--- src/components/store/testStore/testingSaga.ts
+++ src/components/store/testStore/testingSaga.ts
@@ -1,10 +1,11 @@
 import { call, put, takeEvery } from "redux-saga/effects";
+import { CatBreed } from "../../../types";
 
 import { dataSuccess } from "./testStoreSlice";
 
-function* getDataWorker(): Generator<unknown, void, unknown> {
-  const data = yield call(() => fetch("https://api.thecatapi.com/v1/breeds"));
-  const formmatedData = yield data.json();
+function* getDataWorker(): Generator<unknown, void, any> {
+  const response:Response = yield call(() => fetch("https://api.thecatapi.com/v1/breeds"));
+  const formmatedData:CatBreed[] = yield response.json();
   const formmatedDataSliced = formmatedData.slice(0, 10);
   yield put(dataSuccess(formmatedDataSliced));
 }
